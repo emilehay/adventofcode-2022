f2953fd98dde7269eac3b417661628430cd9bb12
emilehay <emilehay@gmail.com>
2022-12-19 10:46:16 +0200

    Completed Day 6, Part 2

---
+++ day6/part2/main.js
@@ -0,0 +1,40 @@
+let input
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                input = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function solveDay6(){
+    let inputArray;
+    readTextFile("./data.txt");
+    //Make array from the string
+    inputArray = input.split('')
+
+    for(let i = 0; i < inputArray.length; i++){
+        subArray = inputArray.slice(i, i + 14);
+        if(!duplicatesFound(subArray)){
+            return i + 14;
+        }
+    }
+}
+
+function duplicatesFound(arr){
+    let arrSet = [...new Set(arr)];
+    if(arrSet.length === arr.length){
+        console.log(arr, arrSet);
+    }
+    return (arrSet.length !== arr.length) ? true : false;
+}
+
+console.log(solveDay6());
